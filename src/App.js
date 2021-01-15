8c1db6f281165b94f768f35727da7b5de2a2927d
Wahyu Ramadhan <ssjrama@gmail.com>
2021-01-15 13:02:29 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
+import Setup from "./tutorial/12-memo-useMemo-useCallback/setup";
+
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
